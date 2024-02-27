083f50dc119ba5391dc8f162bd9f1c3bb42306ff
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-02-27 11:19:28 +0700

    Add project files.

---
+++ Pertemuan 1 Kelas D/Pertemuan 1 Kelas D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{//begin
+	//numeric nAlas, nTinggi, nLuas
+	//display 'Masukan Alasnya'
+	//accept nAlas
+	//display 'Masukan Tingginya= '
+	//accepet nTinggi
+	//compute nLuas = 1/1 * nAlas * nTinggi
+	//display 'Luasnya adalah = ' + nLuas
+	int nAlas, nTinggi;
+	double nLuas;
+	cout << "Masukan Alasnya= ";
+	cin >> nAlas;
+	cout << "Masukan Tingginya= ";
+	cin >> nTinggi;
+	nLuas = 0,5 * nAlas * nTinggi;
+	cout << "Luasnya adalah= " << nLuas << endl;
+
+
+	system("pause");
+}
